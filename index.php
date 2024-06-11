ec21cc6a2447957abc87c968f3a98c776e79cf61
Alok Singh <aloksingh15122004@gmail.com>
2024-06-11 17:10:29 +0530

    updated

--- index.php
+++ index.php
@@ -133,19 +133,12 @@
 									</p>
 									
 
-									<p>IIT Patna invites the recruiters to the Campus Recruitment Programme and gives this batch of graduating students an opportunity to prove themselves and add value to the organisations they join. We hope this will be a pleasant experience for the recruiters and a step forward in improving upon the past relations and building up new ones</p>
-									
+									<p>IIT Patna invites the recruiters to the Campus Recruitment Programme and gives this batch of graduating students an opportunity to prove themselves and add value to the organisations they join.
 
-<p>
-I welcome you to become a part of our story and to form a long-lasting association with the institute.
-</p>
 
-<p> 
+                                       </p>
+									   <a href="message.php">Read More</a>
 
-Prof. T.N. Singh,<br>
-Director, Indian Institute of Technology Patna<br> 
-</p>
-						
 								</div>
 
 							</div>
